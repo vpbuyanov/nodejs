f720b1a0004c8b9ed5af5891ca1557f48537d718
mors-e <mors@nemors.ru>
2023-04-13 13:01:40 +0300

    add example config

--- src/server/handlers.js
+++ src/server/handlers.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {getAllStats, getMainText, getComments} from '../services/service.js'
+import {getAllStats, getMainText, getComments} from '../controllers/controller.js'
 import {inputValidationMiddleware} from "../middlewares/middleware.js";
 
 const handlers = Router()
