4fe237f87e48116ea3a7ab2e92c02b9c0dab86ce
Leonid Bychkov <bychkov.l47@mail.ru>
2023-05-16 22:39:39 +0300

    fixed headers problem

--- src/server/http/server.js
+++ src/server/http/server.js
@@ -75,6 +75,12 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.resolve(__dirname, 'public')))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
 app.use(
     "/api/v1",
     AuthorizationMiddleware,
