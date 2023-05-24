ff7a31925adc2809eac8d541e87b4b305dc3c3c9
mors-e <mors@nemors.ru>
2023-05-24 22:29:39 +0300

    docker

--- src/server/http/api/v1/handlers.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import {getMainText} from '../../../../controllers/mainControllers.js'
-
-
-const handlers = Router()
-
-handlers.get('/', getMainText)
-
-export default handlers
